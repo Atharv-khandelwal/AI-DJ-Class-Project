92c2642e2f084b6f11f23185e544d1f2dc1d6892
Atharv-khandelwal <83112922+Atharv-khandelwal@users.noreply.github.com>
2021-05-07 11:33:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,91 @@
+scoreRightWrist = 0
+scoreLeftWrist = 0
+song = ''
+leftWristX = 0
+leftWristY = 0
+rightWristX = 0
+rightWristY = 0
+function preload() {
+    song = loadSound('believer_remix.mp3')
+}
+
+function setup() {
+    canvas = createCanvas(500, 600)
+    canvas.center()
+
+    video = createCapture(VIDEO)
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose', gotPoses)
+}
+
+function draw() {
+    image(video, 0, 0, 500, 600)
+    if (scoreRightWrist > 0.2) {
+
+        circle(leftWristX, rightWristY, 20)
+        if (rightWristY > 0 && rightWristY <= 100) {
+            document.getElementById('speed').innerHTML = "Speed = 2.5x"
+            song.rate(2.5)
+        }
+
+        else if (rightWristY > 100 && rightWristY <= 200) {
+            document.getElementById('speed').innerHTML = "Speed = 2x"
+            song.rate(2)
+        }
+
+        else if (rightWristY > 200 && rightWristY <= 300) {
+            document.getElementById('speed').innerHTML = "Speed = 1.5x"
+            song.rate(1.5)
+        }
+
+        else if (rightWristY > 300 && rightWristY <= 400) {
+            document.getElementById("speed").innerHTML = "Speed = 1x"
+            song.rate(1)
+        }
+        else if (rightWristY > 400 && rightWristY <= 500) {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x"
+            song.rate(0.5)
+        }
+    }
+    fill("#Faebd7")
+    stroke("#Faebd7")
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20)
+        inNumberleftWristY = Number(leftWristY);
+        remove_deciimals = floor(inNumberleftWristY)
+        volume = remove_deciimals / 500
+        document.getElementById('volume').innerHTML = "volume" + volume
+        song.setVolume(volume)
+    }
+}
+
+function play() {
+    song.play()
+    song.setVolume(1)
+    song.rate(1)
+
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
+
+}
+
+function gotPoses(results) {
+    if (results > 0) {
+        console.log(results)
+        scoreRightWrist = results[0].pose.keypoints[10].score
+        scoreLeftWrist = results[0].pose.keypoints[9].score
+        console.log('scoreLeftWrist=' + scoreLeftWrist)
+
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.leftWrist.y
+        console.log('leftWristX' + leftWristX, 'leftWristY' + leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x
+        rightWristY = results[0].pose.leftWrist.y
+        console.log('rightWristX' + rightWristX, 'rightWristY' + rightWristY)
+    }
+}
